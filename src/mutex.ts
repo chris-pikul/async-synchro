3805101c9ac7488331ae8f1c46893b4fee4c4b5e
Chris Pikul <chris@workmaple.com>
2022-03-09 19:31:49 -0800

    commenting for Mutex

--- src/mutex.ts
+++ src/mutex.ts
@@ -42,6 +42,9 @@ export interface MutexOptions {
   errorCancelled ?: (Error|SynchroError);
 };
 
+/**
+ * Single-user concurrency lock. Only allows one lock at a time.
+ */
 export default class Mutex {
   /**
    * Options dictating how this mutex will work
@@ -58,6 +61,16 @@ export default class Mutex {
    */
   #queue:Array<QueuedPromise>;
 
+  /**
+   * @param options Additional settings for Mutex operation.
+   * @param options.onLock Function which is called when a lock is acquired.
+   * @param options.onRelease Function which is called whenever a lock is
+   * released/unlocked
+   * @param options.onCancel Function called when all the locks have been
+   * cancelled by `cancelAll`.
+   * @param options.errorCancelled Error object that is thrown when the promises
+   * are rejected via `cancelAll`.
+   */
   constructor(options?:MutexOptions) {
     // Bind methods
     this.lock = this.lock.bind(this);
@@ -79,6 +92,52 @@ export default class Mutex {
     return this.#locked;
   }
 
+  /**
+   * Attempts to lock this Mutex, or wait until the previous locks are released.
+   * The promise returned resolves giving a function to call to release/unlock
+   * the mutex.
+   * 
+   * After locking this Mutex, you **must call the releaser** function or else
+   * the waiting locks will never be resolved, potentially locking your
+   * execution thread later. It is suggested that you use the guarded lock
+   * method {@link Mutex.guard} instead, as this will automatically lock and 
+   * release for you.
+   * 
+   * In the event that a waiting lock is cancelled via {@link Mutex.cancelAll},
+   * then the promises will be rejected with the error set either in the
+   * arguments of the `cancelAll` method, the one set in the Mutex constructor
+   * options, or {@link ErrCancelled} if none of the others are set.
+   * 
+   * ---------------------------------------------------------------------------
+   * 
+   * ```
+   * // With Async/Await
+   * try {
+   *    const unlock = await mtx.lock();
+   * 
+   *    // Lock is acquired and work can be performed
+   *    ...
+   * 
+   *    unlock();
+   * } catch(err) {
+   *    // The lock was cancelled
+   * }
+   * 
+   * // With Promises
+   * mtx.lock()
+   *  .then(unlock => {
+   *    // Lock is acquired and work can be performed
+   *    ...
+   *    unlock();
+   *  }).catch(err => {
+   *    // The lock was cancelled
+   *  });
+   * ```
+   * 
+   * ---------------------------------------------------------------------------
+   * 
+   * @returns Promise resolving with the unlock releaser
+   */
   lock():Promise<Releaser> {
     const wasLocked = this.isLocked;
 
@@ -96,19 +155,84 @@ export default class Mutex {
     return prom;
   }
 
+  /**
+   * Performs lock acquisition and unlock/releasing for you.
+   * 
+   * When the lock is achieved (possibly after others have unlocked), the
+   * callback function will be executed. Any value returned by the callback will
+   * be passed through when the promise resolves.
+   * 
+   * Any errors thrown during the callbacks execution will **not** be caught by 
+   * this wrapper, and will bubble up accordingly. This includes the one thrown
+   * by calling {@link Mutex.cancelAll}.
+   * 
+   * ---------------------------------------------------------------------------
+   * 
+   * ```
+   * // With Async/Await
+   * try {
+   *    const results = await mtx.guard(() => {
+   *      // Lock is acquired, perform work
+   *      ...
+   * 
+   *      // Return if you wish for convienience
+   *      return 42;
+   *    });
+   * 
+   *    console.log(results); // 42
+   * } catch(err) {
+   *    // Lock was cancelled
+   * }
+   * 
+   * // With Promises
+   * mtx.guard(() => {
+   *    // Lock is acquired, perform work
+   *    ...
+   * 
+   *    // Return if you wish for convienience
+   *    return 42;
+   * }).then(results => {
+   *    console.log(results); // 42
+   * }).catch(err => {
+   *    // Lock was cancelled
+   * });
+   * ```
+   * 
+   * ---------------------------------------------------------------------------
+   * 
+   * @param cb Callback function executed when the lock is acquired
+   * @returns Promise resolving to the results returned by the callback
+   */
   async guard<T = any>(cb:MutexLockCB<T>):Promise<T> {
+    // Perform the standard locking
     const release = await this.lock();
 
+    // Setup a variable for the return results
     let value:T;
     try {
+      // Use the callback as an async function
       value = await cb();
     } finally {
+      // Always release the lock, regardless of errors
       release();
     }
 
+    // Return the results from the callback
     return value;
   }
 
+  /**
+   * Cancels all waiting locks on this Mutex by rejecting their promises.
+   * 
+   * The error given in the rejection is one of the following (whichever applies
+   * first):
+   * 
+   * - The `err` parameter.
+   * - The `errorCancelled` option set in the Mutex constructor options.
+   * - The {@link ErrCancelled} default error.
+   * 
+   * @param err Optional custom error to throw
+   */
   cancelAll(err?:Error):void {
     // Reject all of the waiting promises and empty the queue
     this.#queue.forEach(({ reject }) => reject(err ?? this.options.errorCancelled ?? ErrCancelled));
@@ -123,8 +247,10 @@ export default class Mutex {
   }
 
   #enque(resolve:LockResolver, reject:LockRejector):void {
+    // By adding to the queue we are locking by nature
     this.#locked = true;
 
+    // Add to the end of the queue the promise functions
     this.#queue.push({
       resolve,
       reject,
@@ -146,6 +272,7 @@ export default class Mutex {
   }
 
   #makeReleaser():Releaser {
+    // Cache the released state
     let released = false;
 
     return ():void => {
